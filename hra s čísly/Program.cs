38a3944114de08d46663cb44411951a10b5354cb
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2021-11-21 21:04:29 +0100

    Přidejte soubory projektu.

---
+++ hra s čísly/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hra_s_čísly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool neuhodl = true;
+            Console.Write("Napiš číslo, které bude muset PC uhádnout");
+            int cislo = int.Parse(Console.ReadLine());
+            while (neuhodl)
+            {
+                int randomCislo = random.Next(1, 50);
+                Console.WriteLine(randomCislo);
+
+                if(randomCislo ==cislo)
+                {
+                    Console.WriteLine("počítači uhodl si číslo");
+                    neuhodl = false;
+
+
+                }
+                else
+                {
+                    Console.WriteLine("počítači neuhodl si číslo");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
